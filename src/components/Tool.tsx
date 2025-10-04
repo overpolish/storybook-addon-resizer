4dd0a95f05c7b54c85c5730ff61ce7f9c9700b18
Dom <dom@overpolish.co>
2025-10-04 20:24:28 +0100

    Adjust story frame size

--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -1,40 +1,122 @@
-import React, { memo, useCallback, useEffect } from "react";
-import { useGlobals, type API } from "storybook/manager-api";
-import { IconButton } from "storybook/internal/components";
-import { ADDON_ID, KEY, TOOL_ID } from "../constants";
-import { LightningIcon } from "@storybook/icons";
+import React, {
+  memo,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+import { useChannel, type API } from "storybook/manager-api";
+import { IconButton, WithTooltip } from "storybook/internal/components";
+import { TOOL_ID } from "../constants";
+import { TransferIcon } from "@storybook/icons";
+import { Slider } from "./Slider";
+import { STORY_RENDERED } from "storybook/internal/core-events";
+import { css, styled } from "storybook/internal/theming";
+
+const IFRAME_ID = "storybook-preview-iframe";
+const ROOT_ID = "storybook-root";
+const PADDED_CLASS = "sb-main-padded";
+
+const PADDING = 32;
+
+const Tooltip = styled.div`
+  padding: 8px;
+  width: 150px;
+`;
 
 export const Tool = memo(function MyAddonSelector({ api }: { api: API }) {
-  const [globals, updateGlobals, storyGlobals] = useGlobals();
+  const [isOpen, setIsOpen] = useState(false);
+  const [width, setWidth] = useState(Number.MAX_SAFE_INTEGER);
+  const [maxWidth, setMaxWidth] = useState(Number.MAX_SAFE_INTEGER);
 
-  const isLocked = KEY in storyGlobals;
-  const isActive = !!globals[KEY];
+  const maxWidthRef = useRef(maxWidth);
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
 
-  const toggle = useCallback(() => {
-    updateGlobals({
-      [KEY]: !isActive,
-    });
-  }, [isActive]);
+  const updateWidthLimits = () => {
+    if (iframeRef.current) {
+      const hasPadding =
+        iframeRef.current.contentDocument?.body.classList.contains(
+          PADDED_CLASS,
+        );
+
+      const iframeWidth =
+        iframeRef.current.clientWidth - (hasPadding ? PADDING : 0);
+
+      setWidth((prevWidth) =>
+        iframeWidth < prevWidth || prevWidth === maxWidthRef.current
+          ? iframeWidth
+          : prevWidth,
+      );
+
+      setMaxWidth(iframeWidth);
+    }
+  };
+
+  useChannel(
+    {
+      [STORY_RENDERED]: () => {
+        const iframe = document.getElementById(IFRAME_ID) as HTMLIFrameElement;
+        iframeRef.current = iframe;
+
+        updateWidthLimits();
+      },
+    },
+    [width, maxWidth],
+  );
+
+  useLayoutEffect(() => {
+    window.addEventListener("resize", updateWidthLimits);
+
+    return () => {
+      window.removeEventListener("resize", updateWidthLimits);
+    };
+  }, []);
 
   useEffect(() => {
-    api.setAddonShortcut(ADDON_ID, {
-      label: "Toggle Measure [O]",
-      defaultShortcut: ["O"],
-      actionName: "outline",
-      showInMenu: false,
-      action: toggle,
-    });
-  }, [toggle, api]);
+    maxWidthRef.current = maxWidth;
+  }, [maxWidth]);
+
+  useEffect(() => {
+    if (iframeRef.current && iframeRef.current.contentDocument) {
+      const storybookRoot =
+        iframeRef.current.contentDocument.getElementById(ROOT_ID);
+
+      if (storybookRoot) {
+        storybookRoot.style.width = `${width}px`;
+      }
+    }
+  }, [width]);
 
   return (
-    <IconButton
-      key={TOOL_ID}
-      active={isActive}
-      disabled={isLocked}
-      title="Enable my addon"
-      onClick={toggle}
+    <WithTooltip
+      placement="bottom"
+      trigger="click"
+      closeOnOutsideClick
+      visible={isOpen}
+      tooltip={
+        <Tooltip>
+          <Slider
+            value={width}
+            maxValue={maxWidth}
+            label="Width"
+            name="width"
+            unit="px"
+            onChange={(value) =>
+              setWidth(Array.isArray(value) ? (value[0] ?? maxWidth) : value)
+            }
+            onReset={() => setWidth(maxWidth)}
+          />
+        </Tooltip>
+      }
+      onVisibleChange={setIsOpen}
     >
-      <LightningIcon />
-    </IconButton>
+      <IconButton
+        key={TOOL_ID}
+        active={width !== maxWidth}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <TransferIcon />
+      </IconButton>
+    </WithTooltip>
   );
 });
